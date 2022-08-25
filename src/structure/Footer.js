4bcf7a83b8bafa1e6db73e9b5cb21d79f8dc890d
zianejaz <zianejaz@gmail.com>
2022-08-25 12:12:42 -0400

    added styles to header and footer and add portfolio links to it.

--- src/structure/Footer.js
+++ src/structure/Footer.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaYoutube, FaDiscord } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex w-full bg-gray-700 text-center text-gray-300 p-5">
+      <div className="w-full">
+        Design and Developed @{" "}
+        <a
+          href="https://www.orbitlogics.com"
+          target="blank"
+          className="text-white font-bold"
+        >
+          Orbit Logics
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://github.com/ZianEjaz"
+          target="blank"
+          className="text-white font-bold"
+        >
+          Zian Ejaz
+        </a>
+      </div>
+      <div className="flex justify-self-end justify-end w-full">
+        <a href="https://github.com/ZianEjaz" className="text-3xl pr-3" target="blank">
+          <FaGithub />
+        </a >
+        <a href="./" className="text-3xl pr-3">
+          <FaYoutube />
+        </a>
+        <a href="./" className="text-3xl pr-3">
+          <FaDiscord />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/structure/Header.js
+++ src/structure/Header.js
@@ -3,12 +3,45 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="signIn">Sign In</Link>
+    <header>
+      <nav class="flex items-center justify-between flex-wrap bg-gray-700 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <Link
+              to="./" class="font-bold text-xl tracking-tight">
+            CRUD Todo App Mern Stack
+          </Link>
+        </div>
+        <div class="block lg:hidden">
+          <button class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-gray-300 hover:border-gray-300">
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-lg lg:flex-grow">
+            <Link
+              to="./"
+              class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 mr-4"
+            >
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="signIn"
+              class="inline-block text-lg px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-gray-600 hover:bg-white mt-4 lg:mt-0"
+            >
+              Sign In
+            </Link>
+          </div>
+        </div>
       </nav>
-    </div>
+    </header>
   );
 };
 
