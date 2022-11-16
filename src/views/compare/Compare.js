4be447782eea0364bc8508b58e5f059daa173084
zianejaz <zianejaz@gmail.com>
2022-11-16 16:24:03 -0500

    compare table working < work on button one click back issue

--- src/views/compare/Compare.js
+++ src/views/compare/Compare.js
@@ -4,7 +4,9 @@ import { CompareContext } from "../../contexts/compare.context";
 
 const Compare = () => {
   const { compareList } = useContext(CompareContext);
-  const titleArray =   Object.keys(typeof( compareList[0]) === 'object' ? compareList[0] : [])
+  const titleArray = Object.keys(
+    typeof compareList[0] === "object" ? compareList[0] : []
+  );
 
   return (
     <div className="flex flex-col">
@@ -14,12 +16,10 @@ const Compare = () => {
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
-
-                 
                   {titleArray.map((heading, index) => {
                     return (
                       <th
-                      key={index}
+                        key={index}
                         scope="col"
                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                       >
@@ -30,59 +30,62 @@ const Compare = () => {
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
-                {
-                  compareList.map((person, index) => (
+                {compareList.map((item, index) => (
+                  <tr key={index}>
                     
-                      <tr key={index} >
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="flex items-center">
-                            <div className="flex-shrink-0 h-10 w-10">
-                              <img
-                                className="h-10 w-10 rounded-full"
-                                src={person.image}
-                                alt=""
-                              />
-                            </div>
-                            <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
-                                {person.name}
-                              </div>
-                              <div className="text-sm text-gray-500">
-                                {person.email}
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-900">
-                            {person.title}
-                          </div>
-                          <div className="text-sm text-gray-500">
-                            {person.department}
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <span
-                            className="px-2 inline-flex text-xs leading-5
-                      font-semibold rounded-full bg-green-100 text-green-800"
-                          >
-                            Active
-                          </span>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {person.role}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <p
-                            
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Edit
-                          </p>
-                        </td>
-                      </tr>
-                    
-                  ))}
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.id}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.title}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        {item.description}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.price}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">
+                        {item.discountPercentage}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.rating}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.stock}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.brand}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{item.category}</div>
+                    </td>
+                    <td>
+                      {" "}
+                      <div className="flex-shrink-0 h-10 w-10">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={item.images[1]}
+                          alt=""
+                        />
+                      </div>
+                    </td>
+                    <td>
+                      {" "}
+                      <div className="flex-shrink-0 h-10 w-10">
+                        <img
+                          className="h-10 w-10 rounded-full"
+                          src={item.images[0]}
+                          alt=""
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
